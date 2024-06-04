ca4749b860d2593d2bddc50511b1a072793b0168
tunk <nguyenkhactu1996@gmail.com>
2024-06-04 19:55:00 +0700

    Initial commit

---
+++ src/main/java/vn/edu/vti/vmall/vmall_mall_service/VmallMallServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_mall_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallMallServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VmallMallServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/vn/edu/vti/vmall/vmall_mall_service/VmallMallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_mall_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmallMallServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
